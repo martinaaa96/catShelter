c62fdc91195516eb69108ca044140fe36230b1d5
Martina Vangelova <mvangelovaa@abv.bg>
2023-07-11 20:24:14 +0300

    first simple example

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const app = express();
 
 app.get('/', (req, res) => {
 
-    res.send('Hello from express');
+    res.send(`<h1> Hello from express </h1> 
+    <a href="/dogs"> Dogs page </a>
+    
+    `);
 });
 
+app.get('/dogs', (req,res)=>{
+    res.send('Dogs page')
+});
+
+app.get('/apple', (req,res)=>{
+    res.send('Apple page')
+})
+
+app.post('/dogs',(req,res)=>{
+    res.send('Dogs received');
+
+})
+
+app.put('dogs',(req,res)=>{
+    res.send('Dogs are deleted')
+})
 app.listen(5000, () => console.log('Server is listening on port 5000...'));
