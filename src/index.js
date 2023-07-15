65602ee1b32471f472f21cd41d675eb44a3508bb
Martina Vangelova <mvangelovaa@abv.bg>
2023-07-15 15:46:13 +0300

    homepage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+
 const config = require('./config');
 const app = express();
 
 
+app.engine('hbs', handlebars.engine());
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
 
-    res.send('Home page');
+    res.render('home', {layout:false});
 });
 
 app.listen(config.PORT, () => console.log(`Server is listening on port ${config.PORT}...`));
