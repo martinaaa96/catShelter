d257f4b9d2fe784bf3677fbc16d2103e69d01dca
Martina Vangelova <mvangelovaa@abv.bg>
2023-07-23 16:24:23 +0300

    set up new url

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 const config = {
     production: {
         PORT: 1234,
-        DB_URI: 'mongodb://127.0.0.1:27017/catsCatalog',
+        DB_URI: 'mongodb://127.0.0.1:27017/catShelter',
     },
     development: {
         PORT: 5000,
-        DB_URI: 'mongodb://127.0.0.1:27017/catsCatalog',
+        DB_URI: 'mongodb://127.0.0.1:27017/catShelter',
     }
 }
 
